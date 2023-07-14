c783d81f1d4058590f7d181caa7534d9f2804b2c
girishyadav <girishayadav.com>
2023-07-14 15:04:55 +0530

    second commit

--- src/components/Family.js
+++ src/components/Family.js
@@ -6,15 +6,15 @@ function Family() {
   return (
     <div className='family-container'>
           <div className='family-together'>
-             <a href='/about'><img src={Members} alt='family' className='family-image'/></a>
+             <a href='/aboutus'><img src={Members} alt='family' className='family-image'/></a>
           </div>         
           <div className='family-content'>
             <h1>
                 Fusing form and function into forever pieces for your home… 
             </h1>
             <p className='family-contents'>Daniel and Sarah Bullen-Webb design and handcraft one-of-a-kind products 
-            using a combination of sustainably sourced wood, resin, acrylic, glass 
-            and metal from their workshop on the border of West Sussex and Surrey. 
+             using a combination of sustainably sourced wood, resin, acrylic, glass 
+             and metal from their workshop on the border of West Sussex and Surrey. 
             </p>
           </div>
     </div>
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-    <div>j</div>
+    <div>
+      <div>
+        huskjh
+      </div>
+    </div>
   )
 }
 
